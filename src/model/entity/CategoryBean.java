e585aed5bb0c858a8453dbcc29f7a3e8018360c7
hiroki-itaya <hiroki.itaya@comsize.com>
2023-05-23 17:54:33 +0900

    CategoryBean TaskBeanの完成形です。

--- src/model/entity/CategoryBean.java
+++ src/model/entity/CategoryBean.java
@@ -5,18 +5,21 @@ public class CategoryBean {
 	/*	カテゴリマスタ m_category のBean*/
 	private int categoryId;
 	private String categoryName;
+
 	public int getCategoryId() {
 		return categoryId;
 	}
+
 	public void setCategoryId(int categoryId) {
 		this.categoryId = categoryId;
 	}
+
 	public String getCategoryName() {
 		return categoryName;
 	}
+
 	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
-
 
 }
--- src/model/entity/TaskBean.java
+++ src/model/entity/TaskBean.java
@@ -2,56 +2,69 @@ package model.entity;
 
 public class TaskBean {
 
-/*	タスクテーブル t_task　のBean*/
+	/*	タスクテーブル t_task のBean*/
 	private int taskId;
 	private String taskName;
 	private int categoryId;
 	private String limitDate;
 	private String userId;
-	private char statusCode;
+	private String statusCode;
 	private String memo;
+
 	public int getTaskId() {
 		return taskId;
 	}
+
 	public void setTaskId(int taskId) {
 		this.taskId = taskId;
 	}
+
 	public String getTaskName() {
 		return taskName;
 	}
+
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
 	}
+
 	public int getCategoryId() {
 		return categoryId;
 	}
+
 	public void setCategoryId(int categoryId) {
 		this.categoryId = categoryId;
 	}
+
 	public String getLimitDate() {
 		return limitDate;
 	}
+
 	public void setLimitDate(String limitDate) {
 		this.limitDate = limitDate;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-	public char getStatusCode() {
+
+	public String getStatusCode() {
 		return statusCode;
 	}
-	public void setStatusCode(char statusCode) {
+
+	public void setStatusCode(String statusCode) {
 		this.statusCode = statusCode;
 	}
+
 	public String getMemo() {
 		return memo;
 	}
+
 	public void setMemo(String memo) {
 		this.memo = memo;
 	}
-
 
 }
