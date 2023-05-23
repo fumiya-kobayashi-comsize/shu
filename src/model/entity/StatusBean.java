dcdabcc5c037f670bb4072e261a6987dc01c52b1
yoshiz <seiwa.yoshizawa@comsize.com>
2023-05-23 17:54:09 +0900

    StatusBeanの完成形です。

---
+++ src/model/entity/StatusBean.java
@@ -0,0 +1,29 @@
+package model.entity;
+
+public class StatusBean {
+
+	private String statusCode;
+
+	private String statusName;
+
+	public StatusBean() {
+
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getStatusName() {
+		return statusName;
+	}
+
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+
+}
