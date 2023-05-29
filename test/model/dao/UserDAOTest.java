b84eee883d8b4aae2b8dd9b52659f6a35d2e4fa7
AikoSakurai0816 <aiko.sakurai@comsize.com>
2023-05-29 13:55:54 +0900

    UserDAOクラスのテストクラス作成

--- test/model/dao/UserDAOTest.java
+++ test/model/dao/UserDAOTest.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 package model.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,26 +9,45 @@ import org.junit.jupiter.api.Test;
 import model.entity.UserBean;
 
 /**
- * @author S-01
- *
+ * UserDAOのテストクラスです。
+ * テストデータの取得や検証を行います。
+ * @author 櫻井藍子
  */
 class UserDAOTest {
 
+	 /**
+     * ユーザのデータを取得するテストケースです。
+     * 正しいユーザIDを指定した場合、正しいデータが返されることを検証します。
+     */
 	@Test
-	void test() {
+	void test1GetUserData() {
 		UserDAO userDAO = new UserDAO();
-		UserBean userData = new UserBean();
-		userData.setUserId("admin");
-		userData.setPassword("2e7d01869cd65058cb884c7c039b804bc76d2a259f6143e84a86980e7cdfe23a");
-		userData.setUserName("桑原");
-		int count = 0;
+		UserBean user = new UserBean();
 		try {
-			count = userDAO.getUserData(userData);
+			user = userDAO.getUserData("admin");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-		assertEquals(1, count);
-
+		assertEquals("admin",user.getUserId());
+		assertEquals("2e7d01869cd65058cb884c7c039b804bc76d2a259f6143e84a86980e7cdfe23a",user.getPassword());
+		assertEquals("桑原",user.getUserName());
 	}
 
+	 /**
+     * ユーザのデータを取得するテストケースです。
+     * 存在しないユーザIDを指定した場合、nullが返されることを検証します。
+     */
+	@Test
+	void test2GetUserData() {
+		UserDAO userDAO = new UserDAO();
+		UserBean user = new UserBean();
+		try {
+			user = userDAO.getUserData(" ");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		assertNull(user.getUserId());
+		assertNull(user.getPassword());
+		assertNull(user.getUserName());
+	}
 }
