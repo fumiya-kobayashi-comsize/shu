ac69f0ead224cb3017508d23c06e85d6ac22c20c
hiroki-itaya <hiroki.itaya@comsize.com>
2023-05-31 16:28:15 +0900

    削除機能DAOのテストクラス作成

--- test/model/dao/TaskDeleteDAOTest.java
+++ test/model/dao/TaskDeleteDAOTest.java
@@ -2,9 +2,13 @@ package model.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.sql.Date;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -14,73 +18,74 @@ import model.entity.TaskCategoryStatusBean;
 class TaskDeleteDAOTest {
 	@Disabled
 	@Test
-	void testselectTask() {
-
-		TaskDeleteDAO taskDeleteDAO = new TaskDeleteDAO();
-		TaskCategoryStatusBean task = new TaskCategoryStatusBean();
-
+	void testDeleteItem() {
+		TaskDeleteDAO TaskDeleteDAO = new TaskDeleteDAO();
+		int count = 0;
 		try {
-			task = taskDeleteDAO.selectTask(1);
+			count = TaskDeleteDAO.deleteTask(9);
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		assertEquals(1, bean.getItemCode());
-		assertEquals("テストDVD1", bean.getItemName());
-		assertEquals("DVD", bean.getCategoryName());
-		assertEquals(3024, bean.getPrice());
+		assertEquals(1, count);
 	}
 
-
-			while (res.next()) {
-
-				TaskCategoryStatusBean task = new TaskCategoryStatusBean();
-
-				String taskName = res.getString("task_name");
-				int categoryId = res.getInt("category_id");
-				String categoryName = res.getString("category_name");
-				Date limitDate = res.getDate("limit_date");
-				String userId = res.getString("user_id");
-				String statusCode = res.getString("status_code");
-				String statusName = res.getString("status_name");
-				String memo = res.getString("memo");
-
-				task.setTaskId(taskId);
-				task.setTaskName(taskName);
-				task.setCategoryId(categoryId);
-				task.setCategoryName(categoryName);
-				task.setLimitDate(limitDate);
-				task.setUserId(userId);
-				task.setStatusCode(statusCode);
-				task.setStatusName(statusName);
-				task.setMemo(memo);
-
-				selectTask.add(task);
+	@Test
+	void testselectTask() {
+		TaskCategoryStatusBean taskCategoryStatusBean = new TaskCategoryStatusBean();
+		List<TaskCategoryStatusBean> selectTask = new ArrayList<TaskCategoryStatusBean>();
+		TaskDeleteDAO taskDeleteDAO = new TaskDeleteDAO();
+		//ファイル読み込みで使用する３つのクラス
+		FileInputStream fi = null;
+		InputStreamReader is = null;
+		BufferedReader br = null;
+		try {
+			selectTask = taskDeleteDAO.selectTask(3);
+			//読み込みファイルのインスタンス生成
+			//ファイル名を指定する
+			fi = new FileInputStream("test2.csv");
+			is = new InputStreamReader(fi);
+			br = new BufferedReader(is);
+			//読み込み行
+			String line;
+			//読み込み行数の管理
+			int i = 0;
+			//列名を管理する為の配列
+			String[] arr = null;
+			//1行ずつ読み込みを行う
+			while ((line = br.readLine()) != null) {
+				//先頭行は列名
+				if (i == 0) {
+					//カンマで分割した内容を配列に格納する
+					arr = line.split(",");
+				} else {
+					//カンマで分割した内容を配列に格納する
+					String[] data = line.split(",");
+					assertEquals(data[0], selectTask.get(i - 1).getUserId());
+					assertEquals(Integer.parseInt(data[1]), selectTask.get(i - 1).getTaskId());
+					assertEquals(data[2], selectTask.get(i - 1).getTaskName());
+					assertEquals(Integer.parseInt(data[3]), selectTask.get(i - 1).getCategoryId());
+					assertEquals(data[4], selectTask.get(i - 1).getCategoryName());
+					assertEquals(Date.valueOf(data[5]), selectTask.get(i - 1).getLimitDate());
+					assertEquals(data[6], selectTask.get(i - 1).getStatusCode());
+					assertEquals(data[7], selectTask.get(i - 1).getStatusName());
+					assertEquals(data[8], selectTask.get(i - 1).getMemo());
+				}
+				//行数のインクリメント
+				i++;
 			}
-			return selectTask;
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
-	/**
-	 * 指定されたタスクIDのタスク情報を削除して処理件数を返します。
-	 * @param taskId タスクID
-	 * @return 削除処理件数
-	 * @throws SQLException
-	 * @throws ClassNotFoundException
-	 */
 
-	//task_idに対応したt_taskのテーブルを削除するDAO（タスク削除用）
-	public int deleteTask(int taskId) throws SQLException, ClassNotFoundException {
-
-		String sql = "DELETE FROM task_db.t_task WHERE task_id = ?";
-		int processingNumber = 0; //処理件数
-		try (Connection con = ConnectionManager.getConnection();
-				PreparedStatement pstmt = con.prepareStatement(sql);) {
-			// プレースホルダへの値の設定
-			pstmt.setInt(1, taskId);
-			processingNumber = pstmt.executeUpdate();
-		}
-		return processingNumber;
-
-	}
 
 }
